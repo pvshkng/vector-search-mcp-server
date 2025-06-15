95c8c908c793b190d6778537fe02ac7866e2fe4b
Puvish <p.kungsapattarakul@gmail.com>
2025-06-15 12:32:41 +0700

    fix port

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ from typing import Annotated
 
 key_pair = RSAKeyPair.generate()
 
-port = int(os.getenv("PORT", "10000"))
+port = int(os.getenv("PORT", "8000"))
 mcp = FastMCP(name="Vector_Search_MCP_Server", host="0.0.0.0", port=port)
 
 # description="Search for documents using vector embeddings"
